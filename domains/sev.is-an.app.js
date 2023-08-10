90ce7d3091116985ab7e7e8a75b03a3576238ff2
Mai <80142063+Mai1ii@users.noreply.github.com>
2023-08-10 08:54:14 +0800

    Create sev.is-an.app.js

---
+++ domains/sev.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'bio link website',
+  domain: 'is-an.app',
+  subdomain: 'sev',
+  owner: {
+    email: 'yurihsq1@gmail.com',
+  },
+  record: {
+    CNAME: 'cd.bio.link'
+  },
+})
